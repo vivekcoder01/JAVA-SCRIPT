272db00dac3e8623824f275b855f4d904928094b
VIVEKANAND CHAURASIYA <138095506+vivekcoder01@users.noreply.github.com>
2025-08-22 07:24:29 +0000

    change data

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 12456
+let accountEmail = "vivekChaurasiya "
+
+var accountPassword = "1212121"
+accountCity = "Gurgao"
+
+
+let accountName = "vivek"
+accountEmail = "Vk51200@gmail.com"
+accountPassword = "546436"
+accountCity = "Hydrabad "
+
+console.table([accountId, accountEmail,accountPassword,accountName])
