1fa79ac40847d0cb10339caa2332d9c65ff2e47a
VIVEKANAND CHAURASIYA <138095506+vivekcoder01@users.noreply.github.com>
2025-08-24 12:46:14 +0000

    some change

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -21,3 +21,27 @@ let someNumber = 55
 let stringnumber = String(someNumber)
 console.log(stringnumber);
 console.log(typeof stringnumber)
+
+
+// *************OPERATINS************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**5)
+console.log(2%2)
+
+let str1 = "Vivek"
+let str2 = " Chaurasiya"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
