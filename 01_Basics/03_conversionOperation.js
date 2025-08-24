cd24efce5dd09bbbf888afe6711db6a5a9c0e0fc
VIVEKANAND CHAURASIYA <138095506+vivekcoder01@users.noreply.github.com>
2025-08-24 11:54:11 +0000

    Conversion Of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let score =" 33abc"
+
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
+
+let isLoggedIn = "Vivek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> True ;
+// 0=> False;
+//" " => False;
+// "Vivek" = True;
+
+let someNumber = 55
+
+let stringnumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber)
